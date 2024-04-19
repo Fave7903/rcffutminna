098963b191757159cf1d4a00d2caf600c0f05c6a
Favour Solomon <femisolomon15@gmail.com>
2024-04-19 14:23:07 +0100

    first react commit

---
+++ flgproject/src/App.js
@@ -0,0 +1,10 @@
+import Nav from './Nav'
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+    </div>
+  );
+}
+
+export default App;
---
+++ flgproject/src/Nav.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './App.css'
+
+const Nav = () => {
+  return (
+    <div>
+         <nav class="navbar">
+            <ul class="nav-list">
+                <li><a href="#home">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#contact">Contact</a></li>
+                <li><a href="#section1">Section 1</a></li>
+                <li><a href="#section2">Section 2</a></li>
+            </ul>
+        </nav>
+    </div>
+
+   
+  )
+}
+
+export default Nav
